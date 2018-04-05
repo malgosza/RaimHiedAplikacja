070306e4be09ffbe9950967c1690aaebb985556c
Gosia <malgosza.niewiadomska@gmail.com>
2018-04-05 17:17:22 +0200

    dodana druga strona

--- aplikacja.py
+++ aplikacja.py
@@ -7,6 +7,9 @@ aplikacja=Flask(__name__)
 def stronaStartowa():
     return render_template('ankietaStrona1.html')
 
+@aplikacja.route("/drugaStrona")
+def drugaStr():
+    return render_template('ankietaStrona2.html')
 
 if __name__ == '__main__':
     aplikacja.run(debug=True)
